5a9d07eac1193b10ab6c6d5863460f11cea18545
Matthew Renodin <matthew.renodin@gmail.com>
2019-04-05 14:57:34 -0400

    This is just adding a simple module to start out with

---
+++ helloworld/main.cpp
@@ -0,0 +1,13 @@
+# include "../common/Simple_util.h"
+//#include "rtems/printer.h"
+//#include "tmacros.h"
+
+int
+ACE_TMAIN(int argc, ACE_TCHAR *argv[])
+//ACE_TMAIN()
+{
+  //rtems_print_printer_fprintf_putc(&rtems_printer);
+  printf( "Matthew says Hello, World\n" );
+}
+
+
